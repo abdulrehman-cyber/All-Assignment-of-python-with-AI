5a8e8c7395154617fca99df195c845c94c85cf13
abdulrehman-cyber <60708450+abdulrehman-cyber@users.noreply.github.com>
2026-07-14 00:38:45 +0500

    Create Madlibs_Game.py

---
+++ sir haroon  projects/code/Madlibs_Game.py
@@ -0,0 +1,12 @@
+# word game where you create a story by filling in blanks with random words.
+
+adjective1 = input("Enter an adjective (description of something): ")
+noun1 = input("Enter a noun (person, place or thing): ")
+adjective2 = input("Enter an adjective (description of something): ")
+verb1 = input("Enter a verb (that ends with ing): ")
+adjective3 = input("Enter an adjective (description of something): ")
+
+print(f"Today, I went to {adjective1} zoo")
+print(f"In an exhibit, I saw {noun1}")
+print(f"{noun1} was {adjective2} and {verb1}")
+print(f"I was {adjective3}")
