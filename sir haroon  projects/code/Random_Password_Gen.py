1a357c0307126d3ccf948a1921014f66eb1638c0
abdulrehman-cyber <60708450+abdulrehman-cyber@users.noreply.github.com>
2026-07-14 00:38:55 +0500

    Create Random_Password_Gen.py

---
+++ sir haroon  projects/code/Random_Password_Gen.py
@@ -0,0 +1,13 @@
+import random
+import string
+
+pass_len = 8
+
+charValues = string.ascii_letters + string.digits+ string.punctuation
+
+password = ""
+
+for i in range(pass_len):
+    password += random.choice(charValues)
+
+print("Your Random Password is: ", password)
