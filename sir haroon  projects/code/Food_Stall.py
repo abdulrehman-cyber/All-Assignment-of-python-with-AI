660b983d29811ba82208a85622b68712a17dbabe
abdulrehman-cyber <60708450+abdulrehman-cyber@users.noreply.github.com>
2026-07-14 00:38:04 +0500

    Create Food_Stall.py

---
+++ sir haroon  projects/code/Food_Stall.py
@@ -0,0 +1,31 @@
+menu = {
+    "pizza" : 30.0,
+    "burger" : 25.0,
+    "fries" : 10.0,
+    "popcorn" : 50.0,
+    "soda" : 15.0,
+    "noodles" : 40.0
+}
+
+cart = []
+total = 0
+print("------------------MENU------------------")
+
+for key,value in menu.items():
+    print(f"{key:10}: ${value:.2f}")
+print("--------------------------------")
+
+while True:
+    food = input("Select an item or press q to quit: ").lower()
+    if food == "q":
+        break
+    elif menu.get(food) is not None:
+        cart.append(food)
+
+print("------------------YOUR ORDER------------------")    
+for food in cart:
+    total += menu.get(food)
+    print(food, end= " ")
+    
+print()
+print(f"Total is: ${total:.2f}")
