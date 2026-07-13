444bc6d5686313f1459fa977a8605dbf375380cc
abdulrehman-cyber <60708450+abdulrehman-cyber@users.noreply.github.com>
2026-07-14 00:38:09 +0500

    Create LEC1.py

---
+++ sir haroon  projects/code/LEC1.py
@@ -0,0 +1,18 @@
+a = int(input("Enter first number:"))
+b = int(input("Enter second number:"))
+c = a+b
+print(c)
+
+a = int(input("Enter side of square:"))
+b =  int(a*a)
+print("Area of square = ", b)
+
+a = float(input("Enter first floating point number:"))
+b = float(input("Enter second floating point number:"))
+c = (a+b)/2 
+print("The average of two floating point numbers is", c)
+
+a = int(input("Enter first number:"))
+b = int(input("Enter second number:"))
+c = a >=b
+print(c)
