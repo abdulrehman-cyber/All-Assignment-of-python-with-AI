a7b19f59184c6e0ada3603bc333ab4ce369c2442
abdulrehman-cyber <60708450+abdulrehman-cyber@users.noreply.github.com>
2026-07-14 00:22:34 +0500

    Create oop assignment 2.py

---
+++ oops lecture + assignment/oop assignment 2.py
@@ -0,0 +1,11 @@
+class AImModel1():
+    def __init__(self,model_name,accuracy):
+        self.model_name = model_name
+        self.accuracy = accuracy
+
+ai_model = "claude"
+accuracy = 65
+
+print(f"Model {ai_model} is running with {accuracy}% accuracy")
+
+        
