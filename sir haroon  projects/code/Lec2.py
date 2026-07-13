81a6068a1ddc18b6700e6f6de6ea25df727d726c
abdulrehman-cyber <60708450+abdulrehman-cyber@users.noreply.github.com>
2026-07-14 00:38:14 +0500

    Create Lec2.py

---
+++ sir haroon  projects/code/Lec2.py
@@ -0,0 +1,43 @@
+# a = 89
+# print(a)
+
+# str = "I am haroon . I am 23 year old"
+# print(str.endswith("roon")) # return true if string end with substring
+# print(str.capitalize()) # capitalizes 1st char
+# print(str.replace("haroon", "ali")) # replaces all occurrences of old with new
+# print(str.find("h")) # return 1st index of 1st occurrence
+# print(str.count("am")) # counts the occurrence of substr in string
+"""---------------------------------------------------------------------------------"""
+# Write a program to input user's first name and print its length
+# firstname = str(input("Enter Your First Name: "))
+# print("The length of your first name is: ", len(firstname))
+
+# Write a program to count occurrences of $ in a string
+# x = "Hi my name is $. I am $ years old. I live in $. I earn in $$"
+# print(x.count("$"))
+
+# WAP to check if a number entered by the user is odd or even.
+
+# print(type(checkNumber))
+# checkNumber = int(input("Enter any number: "))
+# if(checkNumber%2 == 0):
+#     print("You have entered an even number")
+
+# elif(checkNumber%2 != 0):
+#     print("You have entered an odd number") 
+
+# WAP to find the greatest of 3 numbers entered by the user.
+# checkNumber1 = int(input("Enter first number: "))
+# checkNumber2 = int(input("Enter second number: "))
+# checkNumber3 = int(input("Enter third number: "))
+
+# greatest = max(checkNumber1, checkNumber2, checkNumber3)
+# print("The greatest number is:", greatest)
+
+# WAP to check if a number is multiple of 7 or not
+# checkNumber = int(input("Enter any number: "))
+# if(checkNumber%7 == 0):
+#     print("multiple of 7")
+
+# elif(checkNumber%7 != 0):
+#     print("Not a multiple of 7") 
