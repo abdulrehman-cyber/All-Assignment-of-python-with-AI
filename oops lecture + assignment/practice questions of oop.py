5c7897bd3c00716340a08f0b667f2f1c6b6863c2
abdulrehman-cyber <60708450+abdulrehman-cyber@users.noreply.github.com>
2026-07-14 00:22:44 +0500

    Create practice questions of oop.py

---
+++ oops lecture + assignment/practice questions of oop.py
@@ -0,0 +1,24 @@
+# Question 1
+# class person:
+#     def display_name(self,name):
+#         self.name = name
+#     name = input("enter your name : ")
+# n1=person()
+# n1.display_name(n1.name)
+# print("name : ",n1.name)
+
+# Question 2
+
+# class Calculator:
+#     def __init__(self,num1,num2):
+#         self.num1 = num1
+#         self.num2 = num2
+#     def sum (self):
+#         return self.num1 + self.num2
+# a= int(input("Enter first num : "))
+# b=int(input("enter second num :"))
+
+# obj=Calculator(a,b)
+# result=obj.sum()
+# print(result)
+
