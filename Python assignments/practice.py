f17a33c639ac0ec525419d706d12b5de813c7ff2
abdulrehman-cyber <60708450+abdulrehman-cyber@users.noreply.github.com>
2026-07-21 14:43:52 +0500

    Create practice.py

---
+++ Python assignments/practice.py
@@ -0,0 +1,11 @@
+def cal_sum():
+    a=int(input("Enter the first number : "))
+    b=int(input("Enter the second number : "))
+    sum= a+b
+    return sum
+print(cal_sum())
+
+
+print(cal_sum())
+print(cal_sum())
+print(cal_sum())
