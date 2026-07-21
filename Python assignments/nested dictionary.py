a13e7bfff59e0659a36b4d1bc76adedb0395c41e
abdulrehman-cyber <60708450+abdulrehman-cyber@users.noreply.github.com>
2026-07-21 14:43:48 +0500

    Create nested dictionary.py

---
+++ Python assignments/nested dictionary.py
@@ -0,0 +1,10 @@
+info = { 
+        "id": 1,
+        "name" : "abdul",
+        "is-adult":False,
+"parent-info" : {
+            "father name" : "afzal",
+            "mother name" : "sultana"
+}
+}
+print(info["parent-info"])
