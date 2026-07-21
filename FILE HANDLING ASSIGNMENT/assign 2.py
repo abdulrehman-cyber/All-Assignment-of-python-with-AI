93922623a2c2214be06559c846a56069fe8a46e8
abdulrehman-cyber <60708450+abdulrehman-cyber@users.noreply.github.com>
2026-07-21 22:12:24 +0500

    Delete assign 2.py

--- FILE HANDLING ASSIGNMENT/assign 2.py
+++
@@ -1,8 +0,0 @@
-with open("D:\ training_log.txt", "a") as f:
-    count=1
-    while count <=5:
-        print(f"Epoches{count} : training.......")
-        count+=1
-
-with open("D:\ training_log.txt","r") as f:
-    data=f.read()
