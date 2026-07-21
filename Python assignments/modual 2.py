19a03ee0e743e61a53dd0fadb07c28e24e1f97a2
abdulrehman-cyber <60708450+abdulrehman-cyber@users.noreply.github.com>
2026-07-21 14:43:43 +0500

    Create modual 2.py

---
+++ Python assignments/modual 2.py
@@ -0,0 +1,14 @@
+"""
+    modual is afile containing pre-writen python code (function and varable) that you can 
+    reuse in your own program using the import keyword.
+
+
+"""
+# import math
+# print(math.sqrt(15))
+
+# import random 
+# print(random.randint(1,10))
+
+import datetime
+print(datetime.datetime.now())
