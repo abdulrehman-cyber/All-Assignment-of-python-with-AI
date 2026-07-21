0e8b7181e80253f4fab93d3fc8370fd258104b5f
abdulrehman-cyber <60708450+abdulrehman-cyber@users.noreply.github.com>
2026-07-21 14:43:55 +0500

    Create practice question of lambda function.py

---
+++ Python assignments/practice question of lambda function.py
@@ -0,0 +1,34 @@
+# # QUESTION 1
+# a = int(input("Enter the first num = "))
+# b = int(input("Enter The second number = "))
+# sum = lambda a , b : a+b 
+# print(sum(a,b))
+
+# # QUESTION 2
+# x =int(input("Enter the number = "))
+# square = lambda x : x*x
+# print(square(x))
+
+# # QUESTION 3
+# num = int(input("Enter the number = "))
+# check = lambda num : "Even" if num % 2 == 0 else "odd"
+# print(check(num))
+
+# # QUESTRION 4
+# num = [1,2,3,4,5]
+# square = list(map(lambda x : x*x , num))
+# print(square)
+
+# QUESTION 5
+
+# num = [5,10,15,20,25]
+# add_num= list(map(lambda x : x + 10 , num))
+# print(add_num) 
+
+# # QUESTION 6 
+# num = [1,2,3,4,5,6,7,8,9,10]
+# even = list(filter(lambda x : x % 2 == 0 , num ))
+# print(even)
+
+# QUESTION 7
+num = [5,12,8,20]
