21ad816e11a9b9fca016795bfc819fb728cae88f
abdulrehman-cyber <60708450+abdulrehman-cyber@users.noreply.github.com>
2026-07-21 14:43:58 +0500

    Create practice2.py

---
+++ Python assignments/practice2.py
@@ -0,0 +1,29 @@
+# def greet():
+#     user_name=input("enter your name : ")
+#     print("welcome",user_name)
+# greet()
+
+
+# greet()
+# greet()
+# greet()
+
+
+print("==================================================")
+
+
+def even_odd():
+    number=int(input("Enter The Number : "))
+    if number %2==0:
+        return "even"
+    else:
+        return "odd"
+    
+
+print(even_odd())
+
+print(even_odd())
+print(even_odd())
+print(even_odd())
+
+    
