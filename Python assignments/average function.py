78c95518a8456513c0662479d87757d97a1627b7
abdulrehman-cyber <60708450+abdulrehman-cyber@users.noreply.github.com>
2026-07-21 14:42:38 +0500

    Create average function.py

---
+++ Python assignments/average function.py
@@ -0,0 +1,8 @@
+a=int(input("Enter the first number: "))
+b=int(input("Enter the second number : "))
+c=int(input("Enter the third number :"))
+def cal_avg(a, b, c):
+    avg= a+b+c/2
+    return avg
+print(cal_avg(a,b,c))
+
