66d19caca5e6996a2fd3eb2feaca7bdc57aebe8a
abdulrehman-cyber <60708450+abdulrehman-cyber@users.noreply.github.com>
2026-07-21 14:41:08 +0500

    Delete al  exmple.py

--- al  exmple.py
+++
@@ -1,7 +0,0 @@
-accuracy=0
-while accuracy< 100:
-    accuracy+= 10
-    print("training ....Accuracy:", accuracy,"%")
-    if accuracy >=90:
-        print("AI model is ready! Training stopped.")
-        break
